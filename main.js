a46dc38abc1ad39894ee7acbd42448d230b34526
yoshi <yoshi@tech-camp-9-021-no-MacBook-Air.local>
2017-05-23 17:09:58 +0900

    インクリメンタルサーチまで実装

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange','clementine','mandarine','tangerine','papaya','passionfruit','peach','pear','persimmon','physalis','plum/prune','pineapple','pomegranate','raspberry','rambutan','star fruit','strawberry'];
 
-$(function(){
+$(function() {
   var list = $("#list");
 
   function appendList(word){
@@ -8,11 +8,18 @@ $(function(){
     list.append(item);
   }
 
-  $("#submit").on("click",function(){
-    var input = $("#keyword").val();
-    var reg = new RegExp("^" + input);
+  function editElement(element){
+    var result = "^" + element;
+    return result;
+  }
 
-    // 2回目以降検索時に一度削除してまた追加している
+  $("#keyword").on("keyup",function(){
+    var input = $("#keyword").val();
+    var inputs = input.split(" ");
+    var newInputs = inputs.map(editElement);
+    var reg = RegExp(newInputs.join("|"));
+
+
     $(".list").remove();
 
     $.each(fruits,function(i,fruit){
@@ -20,9 +27,9 @@ $(function(){
         appendList(fruit);
       }
     });
+      if ($(".list").length === 0){
+        appendList("一致するものがありませんでした")
+      }
 
-    if ($(".list").length === 0){
-      appendList("一致するものがありませんでした")
-    }
   });
 });
